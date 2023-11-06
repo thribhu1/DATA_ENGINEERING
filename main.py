13de0e91084677f97a0b3c900bf4a0ff442f69b0
thribhu1 <dabbartj@mail.uc.edu>
2023-11-05 20:30:32 -0600

    Data_validation_pipipeline

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 from textSummarizer.logging import logger
 from textSummarizer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from textSummarizer.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 
-STAGE_NAME = "Data Ingestion stage"
+STAGE_NAME_01 = "Data Ingestion stage"
+STAGE_NAME_02 = "Data Validation stage"
 try:
-  logger.info(f"Starting {STAGE_NAME} pipeline")
+  logger.info(f"Starting {STAGE_NAME_01} and  {STAGE_NAME_02} pipeline")
   data_ingestion = DataIngestionTrainingPipeline()
+  data_validation = DataValidationTrainingPipeline()
   data_ingestion.main()
-  logger.info(f"Finished {STAGE_NAME} pipeline")
+  data_validation.main()
+  logger.info(f"Finished {STAGE_NAME_01} and  {STAGE_NAME_02}  pipeline")
   
 except Exception as e:
   logger.exception(e)
